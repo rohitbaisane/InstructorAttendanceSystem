f1f1b27bc9cd80b276b71a205cf37e178a2df9ce
rohitbaisane <rohitbaisane91@gmail.com>
2024-02-18 18:44:24 +0530

    Add validation in in and out api

--- src/repository/insturctor-repository.js
+++ src/repository/insturctor-repository.js
@@ -1,6 +1,26 @@
 const { InstructorAttendance } = require('../models/index');
 
 class InstructorRepository {
+
+    async getActiveCheckIn(filter) {
+        console.log(filter);
+        const existingCheckIn = await InstructorAttendance.findOne({
+            where: filter
+        });
+        console.log(existingCheckIn);
+        return existingCheckIn;
+    }
+
+    async find(filter) {
+        const sequelizeFilter = {
+            where: filter,
+        }
+        console.log(filter);
+        const instructorCheckInRecord = await InstructorAttendance.findOne(sequelizeFilter);
+        console.log(instructorCheckInRecord);
+        return instructorCheckInRecord;
+    }
+
     async checkIn(instructorCheckInData) {
         const instructorCheckInRecord = new InstructorAttendance(instructorCheckInData);
         await instructorCheckInRecord.save();
--- src/services/instructor-service.js
+++ src/services/instructor-service.js
@@ -1,16 +1,72 @@
 const { InstructorRepository } = require('../repository/index.js');
+const { Op } = require('sequelize');
 
 class InstructorService {
 
     constructor() {
         this.instructorRepository = new InstructorRepository();
     }
+
     async checkIn(employeeCheckInData) {
+
+        //check for existing active check in.
+        const existingCheckIn = await this.instructorRepository.getActiveCheckIn({
+            instructorId: employeeCheckInData.instructorId,
+            isCheckedIn: true,
+        });
+
+        if (existingCheckIn) {
+            throw new Error({
+                message: "There is already existing check in for instructor"
+            });
+        }
+
+        //check for record with overlapping check in and check out time.
+        const overlappingCheckInRecord = await this.instructorRepository.find({
+            checkInTime: {
+                [Op.lte]: employeeCheckInData.checkInTime,
+            },
+            checkOutTime: {
+                [Op.gte]: employeeCheckInData.checkInTime,
+            },
+            instructorId: employeeCheckInData.instructorId,
+        });
+        if (overlappingCheckInRecord) {
+            throw new Error({ message: "There is already existing record with overlapping time" });
+        }
+
         const instructorCheckInRecord = await this.instructorRepository.checkIn(employeeCheckInData);
         return instructorCheckInRecord;
     }
+
     async checkOut(employeeCheckOutData) {
+
         const { instructorId, checkOutTime } = employeeCheckOutData;
+
+        //check for existing active check in.
+        const existingCheckIn = await this.instructorRepository.getActiveCheckIn({ instructorId, isCheckedIn: true });
+        if (!existingCheckIn) {
+            throw new Error({
+                message: "There is no check in exist for give instructor"
+            });
+        }
+        if (existingCheckIn && existingCheckIn.checkInTime.getTime() >= new Date(checkOutTime).getTime()) {
+            throw new Error({ message: "Check in time can't be greater or equal than check out time" });
+        }
+
+        //check for record with overlapping check in and check out time.
+        const overlappingCheckInRecord = await this.instructorRepository.find({
+            checkInTime: {
+                [Op.gt]: existingCheckIn.checkInTime,
+                [Op.lte]: checkOutTime,
+            },
+            instructorId,
+        });
+
+        if (overlappingCheckInRecord) {
+            throw new Error({ message: "There is already existing record with overlapping time" });
+        }
+
         const filter = {
             isCheckedIn: true,
             instructorId,
